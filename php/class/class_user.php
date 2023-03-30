30b70ae66e7829dbf6ffc8c8942ab7ab2eba67f3
mehdi-douib <mehdi.douib@laplateforme.io>
2023-03-30 17:12:02 +0200

    commit

--- php/class/class_user.php
+++ php/class/class_user.php
@@ -1,123 +1,68 @@
 <?php
-    class user 
-        {
-//----------Création des attributs de la classe user           
-            public $bdd = '';
-            public $login = '';
-            public $email = '';
-            public $msg_error = '';
-            public $msg_valid = '';
 
-//----------Crée la connexion à la bdd dès que l'objet est appelé "new user"
-            public function __construct($dbname)
-                {
-                    try
-                        {
-                            $bdd = new PDO("mysql:host=localhost;dbname=$dbname", 'root', '', [PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_OBJ, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);                   
-                            $this->bdd = $bdd;
-                            
-                        }
-                        
-                    catch(PDOException $e)//affiche une erreur si mauvais entrée de la bdd
-                        {
-                            $error = $e->getMessage();
-                            echo $error;
-                        }                    
-                }
+class User
+{
+    private $db;
 
-//----------Ferme la connexion à la bbd
-            public function __destruct()
-                {
-                    $this->bdd = '';
-                }
+    public function __construct($host, $db_name, $username, $password)
+    {
+        $dsn = "mysql:host=$host;dbname=$db_name;charset=utf8mb4";
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
 
-//----------Vérifie que le login n'est pas prit
-            public function issetUser($login)
-                {
-                    $query_issset_user = $this->bdd->query("SELECT * FROM utilisateurs WHERE login='$login'");
-                    $isset_user = $query_issset_user->fetch();
+        try {
+            $this->db = new PDO($dsn, $username, $password, $options);
+        } catch (PDOException $e) {
+            throw new Exception('Connection failed: ' . $e->getMessage());
+        }
+    }
 
-                    return $isset_user;
-                }
+    public function createUser($nom_utilisateur, $mot_de_passe, $email_utilisateur, $adresse_livraison, $adresse_facturation)
+    {
+        $stmt = $this->db->prepare("INSERT INTO Utilisateurs(nom_utilisateur, mot_de_passe, email_utilisateur, adresse_livraison, adresse_facturation)
+                                        VALUES (:nom_utilisateur, :mot_de_passe, :email_utilisateur, :adresse_livraison, :adresse_facturation)");
+        $stmt->bindParam(':nom_utilisateur', $nom_utilisateur);
+        $stmt->bindParam(':mot_de_passe', password_hash($mot_de_passe, PASSWORD_DEFAULT));
+        $stmt->bindParam(':email_utilisateur', $email_utilisateur);
+        $stmt->bindParam(':adresse_livraison', $adresse_livraison);
+        $stmt->bindParam(':adresse_facturation', $adresse_facturation);
+        $stmt->execute();
+    }
 
-//----------Insert dans la bdd les info de l'utilisateur
-            public function register($login, $password, $email)
-                {                    
-                    $insert_user = $this->bdd->prepare("INSERT INTO utilisateurs (login, password, email) VALUES (:login, :password, :email)");
-                    $insert_user->execute([
-                        'login' => $login,
-                        'password' => $password,
-                        'email' => $email
-                    ]);                                                   
-                }      
+    public function getUserById($id)
+    {
+        $stmt = $this->db->prepare("SELECT * FROM Utilisateurs WHERE id_utilisateur = :id");
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        return $stmt->fetch();
+    }
 
-//----------Créée une session["user"] => connecte l'utilisateur
-            public function connect($login, $password)     
-                {                                                
-                            if(!empty($this->issetUser($login)))
-                                {
-                                    if(password_verify($password, $this->issetUser($login)->password))
-                                        {
-                                            $_SESSION["user"] = $this->issetUser($login);     
-                                            header("Location:index.php");                                       
-                                        }    
-                                    else
-                                        $this->msg_error = "Mauvais mot de passe";                                                                                                                                  
-                                }    
-                            else
-                                $this->msg_error = "Ce login n'existe pas";
-                }
+    public function getUserByUsername($nom_utilisateur)
+    {
+        $stmt = $this->db->prepare("SELECT * FROM Utilisateurs WHERE nom_utilisateur = :nom_utilisateur");
+        $stmt->bindParam(':nom_utilisateur', $nom_utilisateur);
+        $stmt->execute();
+        return $stmt->fetch();
+    }
 
-//----------Met à jour les données de l'utilisateur
-            public function updateUser($login, $email, $id, $nw_password ='', $conf_password = '')
-                {              
-                    //Met à jour l'attribut login de la classe
-                    if(empty($this->issetUser($login)) || $login == $_SESSION["user"]->login)
-                        {
-                            $this->login = $login;                           
-                        }
-                    else
-                        {
-                            $this->msg_error = "Ce login est déjà prit";
-                        }
-                    //Met à jour l'attribut email de la classe
-                    if(empty($this->issetUser($email)) || $email == $_SESSION["user"]->email)
-                        {                           
-                            $this->email = $email;                         
-                        }
-                    else
-                        {
-                            $this->msg_error = "Cet email existe déjà";
-                        }                    
-                    //Met à jour le password de l'utilisateur
-                    if(isset($nw_password) && !empty($nw_password))
-                        {
-                            if($nw_password == $conf_password)
-                                {
-                                    $hash_nw_password = password_hash($nw_password, PASSWORD_DEFAULT);
+    public function updateUser($id_utilisateur, $nom_utilisateur, $email_utilisateur, $adresse_livraison, $adresse_facturation)
+    {
+        $stmt = $this->db->prepare("UPDATE Utilisateurs SET nom_utilisateur = :nom_utilisateur, email_utilisateur = :email_utilisateur, adresse_livraison = :adresse_livraison, adresse_facturation = :adresse_facturation WHERE id_utilisateur = :id_utilisateur");
+        $stmt->bindParam(':id_utilisateur', $id_utilisateur);
+        $stmt->bindParam(':nom_utilisateur', $nom_utilisateur);
+        $stmt->bindParam(':email_utilisateur', $email_utilisateur);
+        $stmt->bindParam(':adresse_livraison', $adresse_livraison);
+        $stmt->bindParam(':adresse_facturation', $adresse_facturation);
+        $stmt->execute();
+    }
 
-                                    $update_password = $this->bdd->prepare("UPDATE utilisateurs SET password=? WHERE id=?");
-                                    $update_password->execute([
-                                        $hash_nw_password,
-                                        $id
-                                    ]);
-                                    $_SESSION["user"]->password = $hash_nw_password;                                                                      
-                                }
-                            else
-                                {
-                                    $this->msg_error = "Les mots de passe ne correspondent pas";
-                                }
-                        }        
-                    //Met à jour le login et l'email de l'utilisateur dans la BDD 
-                    if(empty($this->msg_error))                                   
-                        {                            
-                            $update = $this->bdd->prepare("UPDATE utilisateurs SET login=?, email=? WHERE id=?");
-                            $update->execute([$this->login,$this->email,$id ]);
-                            $_SESSION["user"]->login = $this->login;
-                            $_SESSION["user"]->email = $this->email;
-
-                            $this->msg_valid = "Modification(s) prise(nt) en compte";
-                        }                    
-                }
-        }    
-?>
+    public function deleteUser($id)
+    {
+        $stmt = $this->db->prepare("DELETE FROM Utilisateurs WHERE id_utilisateur = :id");
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+    }
+}
--- php/traitement/php_inscription.php
+++ php/traitement/php_inscription.php
@@ -3,44 +3,56 @@
 
     session_start();
 
-    $user = new user('boutique');
-   
-
-    if(isset($_SESSION["id"]))
-        {
-            header("Location:index.php");
+    <?php
+    // Informations de connexion à la base de données
+    $servername = "Mariadb";
+    $username = "root";
+    $password = "";
+    $dbname = "boutique";
+    
+    // Connexion à la base de données
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // Définition du mode d'erreur PDO à l'exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connexion réussie à la base de données";
+    } catch(PDOException $e) {
+        echo "Erreur de connexion à la base de données: " . $e->getMessage();
+    }
+    
+    // Récupération des données du formulaire
+    $login = $_POST['login'];
+    $email = $_POST['email'];
+    $livraison = $_POST['livraison'];
+    $facturation = $_POST['Facturation'];
+    $password = $_POST['password'];
+    $confirmpassword = $_POST['confirmpassword'];
+    
+    // Vérification si les mots de passe sont identiques
+    if ($password != $confirmpassword) {
+        echo "Les mots de passe ne correspondent pas";
+    } else {
+        // Hashage du mot de passe pour plus de sécurité
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    
+        // Préparation de la requête SQL avec une requête préparée
+        $stmt = $conn->prepare("INSERT INTO Utilisateurs (nom_utilisateur, mot_de_passe, email_utilisateur, adresse_livraison, adresse_facturation) VALUES (:nom_utilisateur, :mot_de_passe, :email_utilisateur, :adresse_livraison, :adresse_facturation)");
+        $stmt->bindParam(':nom_utilisateur', $login);
+        $stmt->bindParam(':mot_de_passe', $hashed_password);
+        $stmt->bindParam(':email_utilisateur', $email);
+        $stmt->bindParam(':adresse_livraison', $livraison);
+        $stmt->bindParam(':adresse_facturation', $facturation);
+    
+        // Exécution de la requête
+        try {
+            $stmt->execute();
+            echo "Inscription réussie";
+        } catch(PDOException $e) {
+            echo "Erreur d'inscription: " . $e->getMessage();
         }
-    else
-        {
-            if(isset($_POST["valid_insc"], $_POST["login"], $_POST["password"], $_POST["conf_password"], $_POST["email"]) && !empty($_POST["login"]) && !empty($_POST["password"]) && !empty($_POST["conf_password"]) && !empty($_POST["email"]))
-                {
-                    $login = $_POST["login"];
-                    $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
-                    $email = $_POST["email"];            
-        
-                    if(empty($user->issetUser($login)))//Appel la fonction dans la classe user => vérifie que le login n'existe pas
-                        {
-                            if($_POST["password"] == $_POST["conf_password"])
-                                {
-                                    if(filter_var($email, FILTER_VALIDATE_EMAIL))//Vérifie que l'email est bien au format mail
-                                        {                                                                      
-                                            $user->register($login, $password, $email);//Insert l'utilisateur dans la BDD                                                            
-                                            header("Location:connexion.php");
-                                        }      
-                                    else
-                                        {
-                                            $msg_error = "email mauvais format";                                     
-                                        }                      
-                                }   
-                            else
-                                {
-                                    $msg_error = "les mots de passe ne correspondent pas";                            
-                                }     
-                        }   
-                    else
-                        {
-                            $msg_error = "login existe déjà";                    
-                        }     
-                }                           
-        }        
-?>    
+    }
+    
+    // Fermeture de la connexion à la base de données
+    $conn = null;
+    ?>
+    
